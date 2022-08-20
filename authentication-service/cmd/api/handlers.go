c774db90d827cf6a977dba46547c4ad7b1336fc4
Mateusz Baranowski <baku13681@gmail.com>
2022-08-20 10:58:03 +0200

    Update handlers with library

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -4,9 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+
+	"github.com/tsawler/toolbox"
 )
 
 func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+
+	var tools toolbox.Tools
 
 	var requestPayload struct {
 		Email    string `json:"email"`
@@ -15,28 +19,28 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	err := app.readJSON(w, r, &requestPayload)
 	if err != nil {
-		app.errorJSON(w, err, http.StatusBadRequest)
+		tools.ErrorJSON(w, err, http.StatusBadRequest)
 		return
 	}
 
 	// Validate the user against the database
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		app.errorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		app.errorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	payload := jsonResponse{
+	payload := toolbox.JSONResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged as %s", user.Email),
 		Data:    user,
 	}
 
-	app.writeJSON(w, http.StatusAccepted, payload)
+	tools.WriteJSON(w, http.StatusAccepted, payload)
 }
