2b2ba58aab036cef2dbec4865cce303271260577
Mateusz Baranowski <baku13681@gmail.com>
2022-08-27 11:13:36 +0200

    Backoff changes

--- front-end/web/main.go
+++ front-end/web/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"embed"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
 
@@ -14,42 +12,33 @@ func main() {
 		render(w, "main.page.html")
 	})
 
-	fmt.Println("Web started on 8081 port!")
-	err := http.ListenAndServe(":8081", nil)
+	fmt.Println("Web started on 80 port!")
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
-//go:embed template
-var templateFS embed.FS
-
 func render(w http.ResponseWriter, s string) {
 	partials := []string{
-		"template/base.layout.html",
-		"template/header.html",
+		"./web/template/base.layout.html",
+		"./web/template/header.html",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("template/%s", s))
+	templateSlice = append(templateSlice, fmt.Sprintf("./web/template/%s", s))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFS(templateFS, templateSlice...)
+	tmpl, err := template.ParseFiles(templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	var data struct {
-		BrokerURL string
-	}
-
-	data.BrokerURL = os.Getenv("BROKER_URL")
-
-	if err := tmpl.Execute(w, data); err != nil {
+	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
